e74f70ac259e0fed74a07816dc78be3b0755f758
Arijan Nikoci <arijan@nikoci.se>
2022-10-27 12:52:00 +0200

    Added basic functionality methods to handle commands for the bot instance

--- src/main/java/se/nikoci/lgdiscordbot/lib/Bot.java
+++ src/main/java/se/nikoci/lgdiscordbot/lib/Bot.java
@@ -4,11 +4,14 @@ import lombok.Data;
 import lombok.NonNull;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.nikoci.lgdiscordbot.lib.command.Command;
 import se.nikoci.lgdiscordbot.lib.command.CommandHandler;
+import se.nikoci.lgdiscordbot.lib.command.CommandType;
 
 import javax.security.auth.login.LoginException;
 import java.util.*;
@@ -34,7 +37,13 @@ public class Bot {
         bots.add(this);
     }
 
-    public void addCommand(Command command){
+    public void setCommandHandler(@NotNull CommandHandler commandHandler){
+        if (this.commandHandler != null) jda.removeEventListener(this.commandHandler);
+        this.commandHandler = commandHandler;
+        jda.addEventListener(commandHandler);
+    }
+
+    public void addCommand(@NotNull Command command){
         //restrict adding multiple with the same name
         for (var entrySet : commands.entrySet()){
             if (entrySet.getKey().equalsIgnoreCase(command.getName())){
@@ -43,16 +52,21 @@ public class Bot {
             }
         }
 
-        //Check for the CommandType and updateSlashCommand accordingly
-
-
+        commands.put(command.getName(), command);
+        updateCommandData();
     }
 
-    private void updateSlashCommand(){
-
+    public void removeCommand(@NotNull Command command){
+        removeCommand(command.getName());
     }
 
-    public Command getCommand(String name){
+    public void removeCommand(@NotNull String commandName){
+        commands.forEach((name, cmd) -> {
+            if (name.equalsIgnoreCase(commandName)) commands.remove(name, cmd);
+        });
+    }
+
+    public Command getCommand(@NotNull String name){
         for (var entrySet : commands.entrySet()){
             if (entrySet.getKey().equalsIgnoreCase(name)){
                 return entrySet.getValue();
@@ -61,4 +75,31 @@ public class Bot {
         return null;
     }
 
+    public void addCommands(Command @NotNull ... commands){
+        for (Command command : commands) addCommand(command);
+    }
+
+    public void removeCommands(Command @NotNull ... commands){
+        for (Command command : commands) removeCommand(command);
+    }
+
+    //Updates command data for jda, only Command obj's that accept slash commands will be updated.
+    private void updateCommandData(){
+        List<CommandData> commandDataList = new ArrayList<>();
+
+        commands.forEach((name, cmd) -> {
+            //Make sure we only select Command objects that accept slash commands
+            if (cmd.getCommandType().contains(CommandType.GUILD_SLASH)
+                    || cmd.getCommandType().contains(CommandType.DM_SLASH)
+                    || cmd.getCommandType().contains(CommandType.ALL)){
+                commandDataList.add(cmd.getCommandData());
+            }
+        });
+
+        getJda()
+                .updateCommands()
+                .addCommands(commandDataList)
+                .queue();
+
+    }
 }
